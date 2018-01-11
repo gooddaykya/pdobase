c50b33078d85b179d012d5c3baa071cac7c61a8f
gooddaykya <makedonskii.net@gmail.com>
2018-01-11 23:24:07 +0200

    Created insert test

--- tests/TestSuitePDOBase.php
+++ tests/TestSuitePDOBase.php
@@ -11,6 +11,7 @@
 
         public function tearDown()
         {
+            $this->db->execQuery('DROP TABLE IF EXISTS test_table');
             $this->db = null;
         }
 
@@ -21,14 +22,41 @@
 
         public function testCreateTable()
         {
-            $cleanup = 'drop table if exists test_table';
-            $create  = 'create table test_table (
-                id int unsigned not null auto_increment,
-                val varchar(10) not null,
-                primary key(id)
-            ) engine=InnoDB default charset utf8-bin';
+            $cleanup = 'DROP TABLE IF EXISTS test_table';
+            $create  = 
+                'CREATE TABLE test_table ' .
+                '(' .
+                    'id INT UNSIGNED NOT NULL AUTO_INCREMENT, ' .
+                    'val VARCHAR(20) NOT NULL, ' .
+                    'PRIMARY KEY (id)' .
+                ') ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_bin';
 
             $this->db->execQuery($cleanup);
             $this->db->execQuery($create);
+        }
+
+        public function insertQueryProvider()
+        {
+            return array(
+                array(
+                    'INSERT INTO test_table (val) VALUES (:val)',
+                    array(':val' => 'test value 1'),
+                    1
+                ),
+                array(
+                    'INSERT INTO test_table (val) VALUES (:val)',
+                    array(':val' => 'test value 2'),
+                    2
+                )                
+            );
+        }
+
+        /**
+        * @dataProvider insertQueryProvider
+        */
+        public function testInsertValue($request, $bindParams, $expected)
+        {
+            $result = $this->db->execQuery($request, $bindParams)('lastInsertId');
+            $this->assertEquals($expected, $result);
         }
     }
