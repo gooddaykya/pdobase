41e705eafd08cf395cacd594328a830812d4854f
gooddaykya <makedonskii.net@gmail.com>
2018-01-23 16:53:22 +0200

    Fixed setUp method. Now test_table is truncated between the tests

--- tests/TestSuitePDOBase.php
+++ tests/TestSuitePDOBase.php
@@ -11,7 +11,7 @@
 
         public function tearDown()
         {
-            $this->db->execQuery('TRUNCATE TABLE test_table');
+            $this->db->execQuery('TRUNCATE TABLE test_table')('lastInsertId');
         }
 
         public function testCreatePDO()
