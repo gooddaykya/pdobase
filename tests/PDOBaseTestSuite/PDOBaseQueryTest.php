3316f13beeb43cf73c8c3866ed59f2ba782c37fa
gooddaykya <makedonskii.net@gmail.com>
2018-03-01 17:27:04 +0200

    Added test: select row by id

---
+++ tests/PDOBaseTestSuite/PDOBaseQueryTest.php
@@ -0,0 +1,30 @@
+<?php
+    namespace gooddaykya\tests\PDOBaseTestSuite;
+
+
+    use gooddaykya\components\PDOBase;
+
+    class PDOBaseQueryTest extends \PHPUnit\Framework\TestCase
+    {
+        private static $db = null;
+
+        public static function setUpBeforeClass()
+        {
+            $requisites = require __DIR__ . './../../credentials.php';
+            self::$db = new PDOBase($requisites);
+        }
+
+        public function testSelectRowByIdFromTable()
+        {
+            $request = 'SELECT val FROM const_table WHERE id = :id';
+            $params = array(
+                ':id' => 5
+            );
+
+            $lazy = self::$db->execQuery($request, $params);
+            $result = $lazy('fetch')->val;
+
+            $this->assertEquals(13, $result);
+            $this->assertFalse($result != 13);
+        }
+    }
