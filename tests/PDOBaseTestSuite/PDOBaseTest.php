44e46cb521fe49f211f12336da7327e70cd21c2e
gooddaykya <makedonskii.net@gmail.com>
2018-03-01 19:40:32 +0200

    Added test: instantiating PDOBase with incorrect credentials throws PODException

--- tests/PDOBaseTestSuite/PDOBaseTest.php
+++ tests/PDOBaseTestSuite/PDOBaseTest.php
@@ -6,16 +6,31 @@
 
     class PDOBaseTest extends \PHPUnit\Framework\TestCase
     {
-        private static $db = null;
+        private static $credentials = null;
+        private $db;
 
         public static function setUpBeforeClass()
         {
-            $requisites = require_once __DIR__ . './../../credentials.php';
-            self::$db = new PDOBase($requisites);
+            self::$credentials = require __DIR__ . './../../credentials.php';
+        }
+
+        public function setUp()
+        {
+            $this->db = new PDOBase(self::$credentials);
         }
 
         public function testObjIsInstanceOfPDOBase()
         {
-            $this->assertInstanceOf(PDOBase::class, self::$db);
+            $this->assertInstanceOf(PDOBase::class, $this->db);
+        }
+
+        public function testWrongCredentials()
+        {
+            $this->expectException(\PDOException::class);
+            $falseCreds = array_merge(array(), self::$credentials);
+            $falseCreds['user'] .= 'wrong';
+
+            $this->expectException(\PDOException::class);
+            $db = new PDOBase($falseCreds);
         }
     }
